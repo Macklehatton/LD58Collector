85fdcaefb41b212362a7d31262b7261637ee265f
Macklehatton <macklehatton@gmail.com>
2025-10-03 21:03:02 -0500

    Point weapon at mouse

--- Scripts/PlayerWeapon.cs
+++ Scripts/PlayerWeapon.cs
@@ -1,6 +1,15 @@
 using Godot;
 using System;
 
-public partial class PlayerWeapon : Node
+public partial class PlayerWeapon : Node2D
 {
+    public override void _Process(double delta)
+    {
+        var camera = GetViewport().GetCamera2D();
+        Vector2 mousePosition = camera.GetGlobalMousePosition();
+        //Vector2 direction = GlobalPosition - mousePosition;
+
+        LookAt(mousePosition);
+        Rotate(Mathf.Pi / 2.0f);
+    }
 }
