29365eaec85f553e2f0ef7f6263aebf1cbf25338
Macklehatton <macklehatton@gmail.com>
2025-10-03 19:50:05 -0500

    Move in physics

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -6,14 +6,14 @@ public partial class PlayerMovement : CharacterBody2D
 {
     [Export] private float speed;
 
-    public override void _Process(double delta)
+    public override void _PhysicsProcess(double delta)
     {
         float yInput = Input.GetAxis("Down", "Up");
         float xInput = Input.GetAxis("Left", "Right");
 
         Vector2 movement = new Vector2(xInput, -yInput);
 
-        Velocity = movement.Normalized() * speed * (float)delta;
+        Velocity = movement.Normalized() * speed;
 
         MoveAndSlide();
     }
