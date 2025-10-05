660985507e4860021aaf5094c08484fea8445734
Macklehatton <macklehatton@gmail.com>
2025-10-05 11:59:41 -0500

    Tile as scene

--- Inventory.cs
+++ Inventory.cs
@@ -5,13 +5,15 @@ using System.Collections.Generic;
 public partial class Inventory : Node
 {
     [Export] private GridContainer gridContainer;
-    [Export] private Control itemTile;
+    [Export] private PackedScene itemTile;
 
     private List<RandomSword> inventory;
 
+    private int selectedIndex;
+
     public void AddItem()
     {
-        Control instance = (Control)itemTile.Duplicate();
+        Control instance = (Control)itemTile.Instantiate();
         gridContainer.AddChild(instance);
     }
 }
